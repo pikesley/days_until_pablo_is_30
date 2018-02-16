1762f7eebf77f4736bc249f55383d79044a6d577
Sam Pikesley <sampi@moo.com>
2018-02-16 16:07:13 +0000

    Add tests

--- spec/days_until_pablo_is_30/days_until_pablo_is_30_spec.rb
+++ spec/days_until_pablo_is_30/days_until_pablo_is_30_spec.rb
@@ -2,14 +2,22 @@ module DaysUntilPabloIs30
   JSON_HEADERS = { 'HTTP_ACCEPT' => 'application/json' }
 
   describe App do
-    it 'serves JSON' do
-      get '/', nil, JSON_HEADERS
-      expect(last_response).to be_ok
-      expect(JSON.parse last_response.body).to eq (
-        {
-          'app' => 'DaysUntilPabloIs30'
-        }
-      )
+    Timecop.freeze DateTime.parse '2018-02-16T12:00:00' do
+      it 'serves HTML' do
+        get '/'
+        expect(last_response).to be_ok
+        expect(last_response.body).to match /159/
+      end
+
+      it 'serves JSON' do
+        get '/', nil, JSON_HEADERS
+        expect(last_response).to be_ok
+        expect(JSON.parse last_response.body).to eq (
+          {
+            'days' => 159
+          }
+        )
+      end
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+require 'timecop'
 require 'coveralls'
 Coveralls.wear_merged!
 
