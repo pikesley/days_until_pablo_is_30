5c2943f14c7fbb404e3d0225d84e2145f4ad89e0
Sam Pikesley <sam.pikesley@gmail.com>
2018-02-17 13:53:58 +0000

    Animation

--- spec/days_until_pablo_is_30/days_until_pablo_is_30_spec.rb
+++ spec/days_until_pablo_is_30/days_until_pablo_is_30_spec.rb
@@ -2,22 +2,16 @@ module DaysUntilPabloIs30
   JSON_HEADERS = { 'HTTP_ACCEPT' => 'application/json' }
 
   describe App do
-    Timecop.freeze DateTime.parse '2018-02-16T12:00:00' do
-      it 'serves HTML' do
-        get '/'
-        expect(last_response).to be_ok
-        expect(last_response.body).to match /159/
-      end
+    it 'serves HTML' do
+      get '/'
+      expect(last_response).to be_ok
+      expect(last_response.body).to match /days remaining until Pablo is 30/
+    end
 
-      it 'serves JSON' do
-        get '/', nil, JSON_HEADERS
-        expect(last_response).to be_ok
-        expect(JSON.parse last_response.body).to eq (
-          {
-            'days' => 159
-          }
-        )
-      end
+    it 'serves JSON' do
+      get '/', nil, JSON_HEADERS
+      expect(last_response).to be_ok
+      expect(JSON.parse last_response.body).to be_a Hash
     end
   end
 end
--- spec/days_until_pablo_is_30/helpers_spec.rb
+++ spec/days_until_pablo_is_30/helpers_spec.rb
@@ -4,8 +4,16 @@ end
 
 module DaysUntilPabloIs30
   describe Helpers do
+    let(:helpers) { TestHelper.new }
+
     it 'has a config' do
       expect(CONFIG).to be_a Hash
+    end
+
+    it 'knows how many days are left' do
+      Timecop.freeze DateTime.parse '2018-02-16T12:00:00' do
+        expect(helpers.days).to eq 159
+      end
     end
   end
 end
